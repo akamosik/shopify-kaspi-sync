b6b5d0912a5b299383ece6e5e7a21b51e9b45118
Yassawe Kainolda <yassawekainolda@Yassawes-MacBook-Pro.local>
2025-05-04 19:41:21 +0500

    structure

---
+++ backend/config/api.js
@@ -0,0 +1,9 @@
+export const shopifyConfig = {
+    endpoint: process.env.SHOPIFY_ENDPOINT,
+    token: process.env.SHOPIFY_TOKEN
+}
+
+export const kaspiConfig = {
+    endpoint: process.env.KASPI_ENDPOINT, 
+    token: process.env.KASPI_TOKEN
+} 
