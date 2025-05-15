1cbd02fd806bfdb49924f51add510152f767fb11
akamosik <yassawe.kainolda@gmail.com>
2025-05-15 14:11:57 +0500

    better error handling

--- app/services/transformProducts.js
+++ app/services/transformProducts.js
@@ -10,60 +10,64 @@ export async function transformProducts(variants){
     const kaspiProducts = [];
 
     for (const variant of variants){
+        try{
+            const kaspiCatogoriesMetafield = variant.metafields.find((item)=>item.key==="category");
 
-        const kaspiCatogoriesMetafield = variant.metafields.find((item)=>item.key==="category");
+            if (!kaspiCatogoriesMetafield){
+                throw new Error(`Product is missing the mandatory 'categories' metafield.`);
+            }
 
-        if (!kaspiCatogoriesMetafield){
-            console.warn(`Product ${variant.product_id} - missing the 'categories' metafield. Skipping.`); 
-            continue;
+            const categories = JSON.parse(kaspiCatogoriesMetafield.value);
+
+            if (!Array.isArray(categories) || categories.length == 0){
+                throw new Error(`Product's category metafield is invalid array or empty array.`);
+            }
+
+            for (const category_title of categories){
+
+                const category_code = await resolveCategory(category_title);
+    
+                const product_id = variant.product_id.split('/').pop();
+                const variant_id = variant.variant_id.split('/').pop();
+
+                const sku = constructIdentifier(variant_id, category_code); 
+                const family_id = constructIdentifier(product_id, category_code);
+
+
+                const title = variant.title.replace(/\s+/g, ' ').trim();
+                const brand = resolveBrand(variant.vendor);
+                const description = cleanDescription(variant.descriptionHtml); 
+
+                const attributes = await mapAttributes(category_code, variant.metafields, variant.options);
+
+                const price = Number(variant.price);
+                const stock = Number(variant.stock);
+
+                const images = variant.media_nodes.map((item)=> ({url: item.image?.url})) || []; 
+
+                kaspiProducts.push(
+                    {
+                        sku,
+                        product_id,
+                        variant_id,
+                        family_id,
+                        title,
+                        brand,
+                        category_code,
+                        description,
+                        attributes,
+                        price,
+                        stock, 
+                        images
+                    }
+                ); 
+            } 
+        }
+        catch(e){
+            console.warn(`(Title: ${variant.title}, Product: ${variant.product_id}, Variant: ${variant.variand_id}) - transformation failed, reason: `, e.message);
+            // implement logging/notification mechanism to warn user to change filled info
         }
 
-        const categories = JSON.parse(kaspiCatogoriesMetafield.value);
-
-        if (!Array.isArray(categories) || categories.length == 0){
-            console.warn(`Product ${variant.product_id} - category field is invalid array or empty. Skipping.`);
-            continue;
-        }
-
-        for (const category_title of categories){
-
-            const category_code = await resolveCategory(category_title);
-   
-            const product_id = variant.product_id.split('/').pop();
-            const variant_id = variant.variant_id.split('/').pop();
-
-            const sku = constructIdentifier(variant_id, category_code); 
-            const family_id = constructIdentifier(product_id, category_code);
-
-
-            const title = variant.title.replace(/\s+/g, ' ').trim();
-            const brand = resolveBrand(variant.vendor);
-            const description = cleanDescription(variant.descriptionHtml); 
-
-            const attributes = await mapAttributes(category_code, variant.metafields, variant.options);
-
-            const price = Number(variant.price);
-            const stock = Number(variant.stock);
-
-            const images = variant.media_nodes.map((item)=> ({url: item.image?.url})) || []; 
-
-            kaspiProducts.push(
-                {
-                    sku,
-                    product_id,
-                    variant_id,
-                    family_id,
-                    title,
-                    brand,
-                    category_code,
-                    description,
-                    attributes,
-                    price,
-                    stock, 
-                    images
-                }
-            ); 
-        } 
     }
     return kaspiProducts;
 }
@@ -182,7 +186,7 @@ async function mapAttributes(category_code, metafields, options){
         }
 
         else{
-            value = metafield.value;
+            value = metafield.value; // my case has only enum and string, no need to handle other types
         }
 
         attributes.push(
@@ -194,81 +198,9 @@ async function mapAttributes(category_code, metafields, options){
         );
     }
 
-    // handle special cases
+    // handle special cases (no info contained in metafields, but can be inferred from other fields)
+    await mapStaticAttributes(attributes, category_code, options);
 
-    // always same values
-
-    attributes.push(
-        {
-            "code": "Shoes*Additional information",
-            "value": "Босоногая Обувь"
-        }
-    );
-
-    attributes.push(
-        {
-            "code": "Shoes*Shoes sole",
-            "value": ["анатомическая"]
-        }
-    );
-
-    attributes.push(
-        {
-            "code": "Shoes*Size features",
-            "value": ["маломерит на 1 размер"]
-        }
-    );
-
-    // Size
-
-    const sizeOption = options.find(op=>op.name==="Размер" || op.name==="Size");
-    const size = sizeOption.value; // i assume it will always exist
-
-    attributes.push(
-        {
-            "code": "Shoes*Size",
-            "value": [size]
-        }
-    );
-
-    attributes.push(
-        {
-            "code": "Shoes*Manufacturer size",
-            "value": size
-        }
-    );
-
-    // Gender
-
-    const genders = await DBProdMap.getAttributeValues("Shoes*Gender", category_code);
-
-    attributes.push(
-        {
-            "code": "Shoes*Gender",
-            "value": [genders[0].value_name]
-        }
-    ); // just get whatever is default gender for category, i.e. Женские туфли -> для женщин
-
-    // Shoes*Model
-
-    const shoeModelLegalValues = await DBProdMap.getAttributeValues("Shoes*Model", category_code);
-
-    const subcategoryMetafield = metafields.find(m => m.key === "subcategory");
-
-    const a = subcategoryMetafield.value;
-
-    const subcategory = a.includes('->') ? a.split('->')[1].trim().toLowerCase() : a; // AI slop
-    
-    const matchingModel = shoeModelLegalValues.find(model => model.value_name === subcategory);
-
-    const shoemodel = matchingModel ? matchingModel.value_name : shoeModelLegalValues[0].value_name;
-
-    attributes.push(
-        {
-            "code": "Shoes*Model",
-            "value": [shoemodel]
-        }
-    );
 
     // check if all mandatory are filled
 
@@ -284,8 +216,84 @@ async function mapAttributes(category_code, metafields, options){
         }
     });
 
-
     return attributes;
+
+}
+
+async function mapStaticAttributes(attributes, category_code, options){
+
+    // SIZE
+    const sizeOption = options.find(op=>op.name==="Размер" || op.name==="Size");
+
+    if(!sizeOption || !sizeOption.value){
+        throw new Error("Product variant does not have shoe size set");
+    }
+    const size = sizeOption.value; 
+
+
+    // GENDER
+    const categoryGenders = await DBProdMap.getAttributeValues("Shoes*Gender", category_code);
+
+    if(!categoryGenders || !categoryGenders[0] || !categoryGenders[0].value_name){
+        throw new Error("Chosen category does not have specified genders");
+    }
+
+    const gender = categoryGenders[0].value_name;
+
+    attributes.push(
+       
+    ); // just get whatever is default gender for category, i.e. Женские туфли -> для женщин
+
+    // MODEL NAME
+    const shoeModelLegalValues = await DBProdMap.getAttributeValues("Shoes*Model", category_code);
+    const subcategoryMetafield = metafields.find(m => m.key === "subcategory");
+
+    if (!subcategoryMetafield || !subcategoryMetafield.value){
+        throw new Error("Subcategory was not selected in product metafields");
+    }
+
+    const temp = subcategoryMetafield.value;
+
+    // string manipulation to convert "Туфли -> Лоферы" to "лоферы"
+
+    const subcategory = temp.includes('->') ? temp.split('->')[1].trim().toLowerCase() : temp; 
+    
+    const matchingModel = shoeModelLegalValues.find(model => model.value_name === subcategory);
+
+    const shoemodel = matchingModel ? matchingModel.value_name : shoeModelLegalValues[0].value_name;
+
+
+
+    attributes.push(
+        {
+            "code": "Shoes*Additional information",
+            "value": "Босоногая Обувь"
+        },
+        {
+            "code": "Shoes*Shoes sole",
+            "value": ["анатомическая"]
+        },
+        {
+            "code": "Shoes*Size features",
+            "value": ["маломерит на 1 размер"]
+        }, 
+        {
+            "code": "Shoes*Size",
+            "value": [size]
+        },
+        {
+            "code": "Shoes*Manufacturer size",
+            "value": size
+        },
+        {
+            "code": "Shoes*Gender",
+            "value": [gender]
+        },
+        {
+            "code": "Shoes*Model",
+            "value": [shoemodel]
+        }
+    );
 
 }
 
