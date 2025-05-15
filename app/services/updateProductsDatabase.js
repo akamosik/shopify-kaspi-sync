3deeee7c82ee54270c036a2c2041311f7163a599
akamosik <yassawe.kainolda@gmail.com>
2025-05-16 00:46:20 +0500

    db

--- app/services/updateProductsDatabase.js
+++ app/services/updateProductsDatabase.js
@@ -1,17 +1,23 @@
 import * as DBProd from "../db/dbproducts.js"
 
 
-export async function updateProductsDatabase(productsList){
+export async function updateProductsDatabase(productList){
 
     const existingProducts = await DBProd.getExistingProducts();
 
+    const existingSKUs = existingProducts.map(p=>p.sku);
+    const incomingSKUs = productList.map(p=>p.sku);
 
+    for (const product of productList){
 
+        if (!existingSKUs.has(product.sku)){
+            // new
 
+        }
+        else{
+            
 
-
-
-
-
+        }
+    }
 
 }
